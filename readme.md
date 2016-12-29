e1caedcb42d7bc3604feaaa46e806b8a93b83c82
Alexander Nelzin <asnelzin@gmail.com>
2016-12-29 17:56:19 +0300

    Add license, update plugin headers for github-updater support;

---
+++ readme.md
@@ -0,0 +1,7 @@
+# wc-api-thumbnails
+
+> This plugin adds image thumbnails to API product responses.
+
+## License
+
+MIT © [Alexander Nelzin](http://asnelzin.ru)
--- wc-api-thumbnails.php
+++ wc-api-thumbnails.php
@@ -1,10 +1,16 @@
 <?php
-/*
-Plugin Name: WC API Thumbnails
-Version:     0.0.3
-Author:      Alexander Nelzin
-Author URI:  https://asnelzin.ru
-*/
+/**
+ * Plugin Name:       WC API Product Thumbnails
+ * Plugin URI:        https://github.com/asnelzin/wc-api-thumbnails
+ * Description:       This plugin adds image thumbnails to API product responses.
+ * Version:           0.0.3
+ * Author:            Alexander Nelzin
+ * License:           MIT License
+ * License URI:       https://opensource.org/licenses/mit-license.php
+ * Text Domain:       wc-api-thumbnails
+ * GitHub Plugin URI: https://github.com/asnelzin/wc-api-thumbnails
+ * GitHub Branch:     master
+ */
 
 add_filter( 'woocommerce_rest_prepare_product', 'custom_products_api_data', 90, 2 );
 function custom_products_api_data( $response, $post ) {
